f5415b522233c1ca14a42003465563dd44f2e032
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-05-21 16:01:33 +0100

    Added the function to see if the sensors are stuck, if the input is out of range, input is random and transform the entries to a single list

--- pyversion/glucose.py
+++ pyversion/glucose.py
@@ -1,9 +1,112 @@
 import math
 
-def blood_glucose(entry_value):
-    #CHECK: range for entry_value
-    if (entry_value < 1 or  entry_value > 5):
-        raise ValueError('entry value must be a number between 1 and 5')
+admitted_percentage = 0.3
 
+def blood_glucose(entry_value):
     glucose_function = -3.4+1.354*entry_value+1.545*math.tan(entry_value**(0.25))
     return round(glucose_function, 5)
+
+def parse_reading(list_of_readings):
+    parsed_list = []
+    for entry in list_of_readings:
+        if (entry[0] == '--' and entry[1] == '--'):
+            parsed_list.append('--')
+        elif entry[0] == '--':
+            parsed_list.append(entry[1])
+        elif entry[1] == '--':
+            parsed_list.append(entry[0])
+        else:
+            parsed_list.append((entry[0] + entry[1])/2)
+    return parsed_list
+
+def out_of_range(entry_value):
+    #CHECK: range for entry_value
+    if (entry_value < 1 or entry_value > 5):
+        return '--'
+    return entry_value
+
+def reading_out_of_range(list_of_readings):
+    for entry in list_of_readings:
+        entry[0] = out_of_range(entry[0])
+        entry[1] = out_of_range(entry[1])
+    return list_of_readings
+
+def reading_stuck_at(list_of_readings):
+    #CHECK: if the two previous valid values are equal to the current value sensor is stuck at that value
+    for i in range(len(list_of_readings)-1, -1, -1):
+        if(i > 1):
+            j = i
+            repeated = 0
+
+            while(j != 0 and repeated < 2):
+                if(list_of_readings[j-1][0] != '--'):
+                    if(list_of_readings[i][0] == list_of_readings[j-1][0]):
+                        repeated += 1
+                    else:
+                        break
+                j-=1
+
+            if(repeated == 2):
+                list_of_readings[i][0] = '--'
+
+            j = i
+            repeated = 0
+
+            while(j != 0 and repeated < 2):
+                if(list_of_readings[j-1][1] != '--'):
+                    if(list_of_readings[i][1] == list_of_readings[j-1][1]):
+                        repeated += 1
+                    else:
+                        break
+                j-=1
+
+            if(repeated == 2):
+                list_of_readings[i][1] = '--'
+
+    return list_of_readings
+
+def reading_random(list_of_readings):
+    #CHECK: if the values are too diferent select the more reliable one if exists
+    for i in range(len(list_of_readings)-1, -1, -1):
+        if(i > 0):
+            if(list_of_readings[i][0] != '--' and list_of_readings[i][1] != '--'):
+                entry_difference = math.fabs(list_of_readings[i][0] - list_of_readings[i][1])
+                if(entry_difference > (list_of_readings[i][0] * admitted_percentage) or entry_difference > (list_of_readings[i][1] * admitted_percentage)):
+                    j = i
+                    while(j != 0):
+                        if(list_of_readings[j-1][0] != '--'):
+                            previous_difference = math.fabs(list_of_readings[i][0] - list_of_readings[j-1][0])
+                            if(previous_difference > (list_of_readings[j-1][0] * admitted_percentage)):
+                                list_of_readings[i][0] = '--'
+                            break
+                        j-=1
+
+                    j = i
+                    while(j != 0):
+                        if(list_of_readings[j-1][1] != '--'):
+                            previous_difference = math.fabs(list_of_readings[i][1] - list_of_readings[j-1][1])
+                            if(previous_difference > (list_of_readings[j-1][1] * admitted_percentage)):
+                                list_of_readings[i][1] = '--'
+                            break
+                        j-=1
+
+            if(list_of_readings[i][0] != '--'):
+                j = i
+                while(j != 0):
+                    if(list_of_readings[j-1][0] != '--'):
+                        previous_difference = math.fabs(list_of_readings[i][0] - list_of_readings[j-1][0])
+                        if(previous_difference > (list_of_readings[j-1][0] * admitted_percentage)):
+                            list_of_readings[i][0] = '--'
+                        break
+                    j-=1
+            if(list_of_readings[i][1] != '--'):
+                j = i
+                while(j != 0):
+                    if(list_of_readings[j-1][1] != '--'):
+                        previous_difference = math.fabs(list_of_readings[i][1] - list_of_readings[j-1][1])
+                        if(previous_difference > (list_of_readings[j-1][1] * admitted_percentage)):
+                            list_of_readings[i][1] = '--'
+                        break
+                    j-=1
+
+    return list_of_readings
--- test/test_glucose.py
+++ test/test_glucose.py
@@ -2,13 +2,25 @@ import unittest
 import pyversion.glucose as glucose
 
 class GlucoseTest(unittest.TestCase):
-    def test_blood_glucose_range(self):
-        self.assertRaises(ValueError, glucose.blood_glucose, 0.9)
-        self.assertRaises(ValueError, glucose.blood_glucose, 5.1)
-
     def test_blood_glucose_function(self):
         self.assertEqual(0.69121, glucose.blood_glucose(1.12345))
         self.assertEqual(3.88247, glucose.blood_glucose(2.23456))
         self.assertEqual(8.0931, glucose.blood_glucose(3.34567))
         self.assertEqual(17.02425, glucose.blood_glucose(4.57653))
         self.assertEqual(16.85495, glucose.blood_glucose(4.56224))
+
+    def test_parse_reading(self):
+        self.assertEqual([1.23456, 1.23456, '--', 2], glucose.parse_reading([['--', 1.23456],[1.23456, '--'],['--', '--'],[3, 1]]))
+
+    def test_reading_out_of_range(self):
+        self.assertEqual([['--', 1], [5, '--'], [1, 5], ['--', '--']], glucose.reading_out_of_range([[0, 1], [5, 6], [1, 5], ['--', '--']]))
+
+    def test_reading_stuck_at(self):
+        self.assertEqual([[0, 1], [0, 2], ['--', 3]], glucose.reading_stuck_at([[0, 1], [0, 2], [0, 3]]))
+        self.assertEqual([[0, 1], [0, 2], ['--', 3], [1, 2]], glucose.reading_stuck_at([[0, 1], [0, 2], [0, 3], [1, 2]]))
+        self.assertEqual([[1, 2], [0, 1], [0, 2], ['--', 3]], glucose.reading_stuck_at([[1, 2], [0, 1], [0, 2], [0, 3]]))
+        self.assertEqual([[0, 2], [0, 1], ['--', 2], ['--', 3]], glucose.reading_stuck_at([[0, 2], [0, 1], ['--', 2], [0, 3]]))
+        self.assertEqual([[1, 2], [0, 1], [1, 2], [0, 3]], glucose.reading_stuck_at([[1, 2], [0, 1], [1, 2], [0, 3]]))
+
+    def test_reading_random(self):
+        self.assertEqual([[1, 1], ['--', '--'], [1, '--']], glucose.reading_random([[1, 1], ['--', 3], [1, 2]]))
