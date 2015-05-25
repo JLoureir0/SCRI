a553fccdaafdbed89be3daca6d70437377f00645
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-05-25 03:37:15 +0100

    Added example script

---
+++ example.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./cppversion/cppVersion input/readings output/v1
+java -jar javaversion/scri.jar input/readings output/v2
+python pyversion/main.py input/readings output/v3
+python voter/voter.py output/v1 output/v2 output/v3 output/dosageython voter/voter.py output/v1 output/v2 output/v3 output/dosage
