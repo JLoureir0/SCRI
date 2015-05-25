9d4f83e76b365e21253e518b3de5a9132086aebe
João Loureiro <joao.a.g.loureiro@gmail.com>
2015-05-25 03:38:37 +0100

    Fixed example.sh and removed dosageython

--- example.sh
+++ example.sh
@@ -2,4 +2,4 @@
 ./cppversion/cppVersion input/readings output/v1
 java -jar javaversion/scri.jar input/readings output/v2
 python pyversion/main.py input/readings output/v3
-python voter/voter.py output/v1 output/v2 output/v3 output/dosageython voter/voter.py output/v1 output/v2 output/v3 output/dosage
+python voter/voter.py output/v1 output/v2 output/v3 output/dosage
